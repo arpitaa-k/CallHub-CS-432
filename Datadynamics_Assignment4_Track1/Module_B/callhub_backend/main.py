7fcf46f592dc7716907d278539f19e7388d243f7
Seemanshi2104 <143334335+Seemanshi2104@users.noreply.github.com>
2026-04-17 04:51:03 +0530

    Update main.py

--- Datadynamics_Assignment4_Track1/Module_B/callhub_backend/main.py
+++ Datadynamics_Assignment4_Track1/Module_B/callhub_backend/main.py
@@ -132,5 +132,5 @@ def portfolio_page():
 if __name__ == "__main__":
     host = os.getenv("FLASK_HOST", "0.0.0.0")
     port = int(os.getenv("FLASK_PORT", "5000"))
-    debug = os.getenv("FLASK_DEBUG", "false").lower() == "true"
+    debug = os.getenv("FLASK_DEBUG", "true").lower() == "true"
     app.run(host=host, port=port, debug=debug)
